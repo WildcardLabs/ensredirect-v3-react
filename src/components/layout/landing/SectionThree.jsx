a5a26c2a580ca2c5b819699bfca9149e5176d0cb
nuex001 <edeemmanuelchizurumoke@gmail.com>
2023-10-16 02:49:47 -0700

    removed some excess files and opted in to use cloudinary  to increase optimization

--- src/components/layout/landing/SectionThree.jsx
+++ src/components/layout/landing/SectionThree.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import sec3Two from "../../../assets/images/sec3-Two.svg"
+// import sec3Two from "../../../assets/images/sec3-Two.svg"
 import sec3One from "../../../assets/images/sec3-One.svg"
 
 function SectionThree() {
@@ -10,7 +10,7 @@ function SectionThree() {
         <h1>Custom <span>MediaHub</span></h1>
         <p>Your favorite streaming platforms on your ENS domain.Generate the ultimate profile that showcases all your video and audio content, seamlessly embedded on your ENS domain. Elevate how your content is discovered - All in one place.</p>
       </div>
-      <img src={sec3Two} alt="" className='coverImg'/>
+      <img src="https://res.cloudinary.com/dtjl9nigz/image/upload/v1697449537/Group_35_kyab34.png" alt="" className='coverImg'/>
     </section>
   )
 }
--- src/components/layout/landing/SectionTwo.jsx
+++ src/components/layout/landing/SectionTwo.jsx
@@ -1,16 +1,15 @@
 import React from 'react'
-import sec2Two from "../../../assets/images/sec2-Two.svg"
 import sec2One from "../../../assets/images/sec2-One.svg"
 function SectionTwo() {
   return (
     <section className="sectionTwo">
-    <div className="box">
+      <div className="box">
         <img src={sec2One} alt="" />
         <h1>ENS<span> Profile</span></h1>
         <p>Curate a personalized profile that represents how you want to be identified online.Manage and showcase your profile with the ability to customize your text records.</p>
-    </div>
-   <img src={sec2Two} alt="" className='coverImg'loading="lazy"/>
-</section>
+      </div>
+      <img src="https://res.cloudinary.com/dtjl9nigz/image/upload/v1697448525/Frame_23_jw0bb7.svg" alt="" className='coverImg' loading="lazy" />
+    </section>
   )
 }
 
